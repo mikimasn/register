1b9a0628f6d4022db91c0906e51ff97525451c6a
pythontest <50592309+mikimasn@users.noreply.github.com>
2021-11-08 18:17:22 +0100

    main

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var port = process.env.PORT || 8000;
 var express=require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var app = express().use(bodyParser.json());
 app.get('/',(req,res)=>{
-    res.send("witaj");
+    res.status(200).send('EVENT_RECEIVED');
 });
-app.listen(port).then(()=>{
-    console.log("Listening: "+port);
-})
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening!'));
